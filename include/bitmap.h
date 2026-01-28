59b0e34ca959101ae9d0f0690ac1eed841967785
Matúš Kadlecay <matus.kadlecay@gmail.com>
2026-01-28 10:10:36 +0100

    small refactoring of bitmap files

--- include/bitmap.h
+++ include/bitmap.h
@@ -5,34 +5,43 @@
 #include <vector>
 #include <fstream>
 
-constexpr std::size_t HIDDEN_METADATA_SIZE = 8;
+const std::size_t hidden_metadata_size = 8;
 
 namespace bmp {
 
 struct image {
     std::string filename;
-    std::ifstream in;
-    std::ofstream out;
+    std::ifstream input;
+    std::ofstream output;
     int32_t width;
     int32_t height;
+    /* 3 for BGR, 4 for BGRA*/
     int16_t channel_count;
 
     int32_t data_offset;
     /* how many bytes can be hidden in total */
     std::size_t byte_capacity;
+    /* search "bitmap padding" on the internet for more info */
     int padding;
+
     /* used for extraction */
     unsigned char id;
     unsigned char seq;
 
-    std::vector<unsigned char> header;
+    std::vector<uint8_t> header;
 
     image(const char *filename) : filename(filename),
-        in(filename, std::ios::binary) {}
+        input(filename, std::ios::binary) {}
 
     image(std::string filename) : filename(filename),
-        in(filename, std::ios::binary) {}
+        input(filename, std::ios::binary) {}
 
+    /**
+     * Opens an output stream for image '.../image.bmp' as
+     * './output_bitmaps/image.bmp.out'
+     * 
+     * @return `true` on success, `false` otherwise
+    */
     bool open_ofstream();
 };
 
@@ -41,18 +50,17 @@ struct image {
  * struct.
  * 
  * @param im reference to image struct, where information is stored
- * @param chunk_count amount of bytes needed to hide a single byte into
+ * @param cells_per_byte amount of bytes needed to hide a single byte into
  * the image, used to count byte_capacity of the image
  * 
  * @return `true` on success, `false` otherwise
  * 
- * @note image struct `im` must have opened ifstream `in` (this should be
+ * @note image struct `im` must have opened ifstream `input` (this should be
  * guaranteed by the image constructor) currently pointing at the beginning
- * of the file. After the call, ifstream `in` is pointing at beginning
+ * of the file. After the call, ifstream `input` is pointing at beginning
  * of bmp's data section.
- * 
  */
-bool read_header(image &im, std::size_t chunk_count);
+bool load_header(image &im, std::size_t cells_per_byte);
 
 }
 
--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -4,17 +4,17 @@
 
 #include "../include/bitmap.h"
 
-const int FST_HEAD_SIZE = 14;
+const int smaller_header_size = 14;
 
 namespace bmp {
 
-static int16_t chars_to_int16(const unsigned char *data)
+static int16_t to_int16(const uint8_t *data)
 {
     return static_cast<int16_t>(data[0]) |
            (static_cast<int16_t>(data[1]) << 8);
 }
 
-static int32_t chars_to_int32(const unsigned char *data)
+static int32_t to_int32(const uint8_t *data)
 {
     return static_cast<int32_t>(data[0]) |
            (static_cast<int32_t>(data[1]) << 8) |
@@ -27,69 +27,69 @@ static int count_padding(int width)
     return (4 - width % 4) % 4;
 }
 
-static std::streamsize get_file_size(std::ifstream &file)
+std::streamsize get_file_size(std::ifstream &file)
 {
-    std::streampos orig = file.tellg();
+    auto orig = file.tellg();
     file.seekg(0, std::ios::end);
-    std::streamsize file_size = file.tellg();
+    auto file_size = file.tellg();
     file.seekg(orig, std::ios::beg);
     return file_size;
 }
 
-bool read_header(image &im, std::size_t chunk_count)
+bool load_header(image &im, std::size_t cells_per_byte)
 {
     using namespace std::string_literals;
 
-    im.header.resize(FST_HEAD_SIZE);
+    im.header.resize(smaller_header_size);
 
-    std::streamsize file_size = get_file_size(im.in);
+    auto file_size = get_file_size(im.input);
 
-    if (file_size < FST_HEAD_SIZE) {
-        std::cerr << "file " << im.filename << "is too small to be bmp\n";
+    if (file_size < smaller_header_size) {
+        std::cerr << "file " << im.filename << " is too small to be bmp\n";
         return false;
     }
-    if (!im.in.read(reinterpret_cast<char *>(im.header.data()), FST_HEAD_SIZE)) {
-        std::cerr << "file " << im.filename << "could not be read\n";
+    if (!im.input.read(reinterpret_cast<char *>(im.header.data()), smaller_header_size)) {
+        std::cerr << "file " << im.filename << " could not be read\n";
         return false;
     }
-    if (im.header[0] != u'B' || im.header[1] != u'M') {
-        std::cerr << "file " << im.filename << "invalid magic number for bmp "
-            "file: " << im.header[0] << im.header[1] << '\n';
+    if (im.header[0] != 'B' || im.header[1] != 'M') {
+        std::cerr << "file " << im.filename << " has invalid magic number to be"
+            " bmp file: " << im.header[0] << im.header[1] << '\n';
         return false;
     }
-    if (chars_to_int32(im.header.data() + 2) != file_size) {
+    if (to_int32(im.header.data() + 2) != file_size) {
         std::cerr << "file " << im.filename << " - the actual size and size "
             "in bmp header does not match\n";
         return false;
     }
-    im.data_offset = chars_to_int32(im.header.data() + 10);
+    im.data_offset = to_int32(im.header.data() + 10);
     im.header.resize(im.data_offset);
-    if (!im.in.read(reinterpret_cast<char *>(im.header.data()) + FST_HEAD_SIZE,
-                    im.data_offset - FST_HEAD_SIZE)) {
-        std::cerr << "file " << im.filename << "could not be read\n";
+    if (!im.input.read(reinterpret_cast<char *>(im.header.data()) + smaller_header_size,
+                    im.data_offset - smaller_header_size)) {
+        std::cerr << "file " << im.filename << " could not be read\n";
         return false;
     }
-    im.width = chars_to_int32(im.header.data() + 18);
-    im.height = chars_to_int32(im.header.data() + 22);
+    im.width = to_int32(im.header.data() + 18);
+    im.height = to_int32(im.header.data() + 22);
 
-    int16_t bit_count = chars_to_int16(im.header.data() + 28);
+    int16_t bit_count = to_int16(im.header.data() + 28);
     if (bit_count != 24 && bit_count != 32) {
-        std::cerr << "file " << im.filename << "invalid bit count for single "
-            "pixel, use 24/32\n";
+        std::cerr << "file " << im.filename << " has invalid bit count "
+            "for single pixel, use 24/32\n";
         return false;
     }
     im.channel_count = bit_count / 8;
     im.padding = count_padding(im.width * im.channel_count);
 
     im.byte_capacity = (static_cast<std::size_t>(im.width) * im.channel_count
-                        - im.padding) * im.height / chunk_count;
-    if (im.byte_capacity <= HIDDEN_METADATA_SIZE * chunk_count) {
-        std::cerr << "file " << im.filename << "size is too small\n";
+                        - im.padding) * im.height / cells_per_byte;
+    if (im.byte_capacity <= hidden_metadata_size) {
+        std::cerr << "file " << im.filename << " is too small to hide data\n";
         return false;
     }
-    int32_t compression = chars_to_int32(im.header.data() + 30);
+    int32_t compression = to_int32(im.header.data() + 30);
     if (compression) {
-        std::cerr << "file " << im.filename << "is compressed\n";
+        std::cerr << "file " << im.filename << " is compressed\n";
         return false;
     }
     return true;
@@ -99,18 +99,18 @@ bool image::open_ofstream()
 {
     using namespace std::string_literals;
 
-    std::size_t basename_index = filename.rfind('/');
+    auto basename_index = filename.rfind('/');
     if (basename_index == std::string::npos)
         basename_index = 0;
     else
         ++basename_index;
-    std::string basename = filename.substr(basename_index);
+    auto basename = filename.substr(basename_index);
 
-    std::filesystem::create_directories("output_bitmaps"s);
+    std::filesystem::create_directories("output_bitmaps");
 
-    out.open("output_bitmaps/"s + basename + ".out"s,
+    output.open("output_bitmaps/"s + basename + ".out"s,
         std::ios::binary | std::ios::trunc);
-    return out.good();
+    return output.good();
 }
 
 }
