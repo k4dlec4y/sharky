0263a575e7413c47637956818b77c22b31ffabb2
Matúš Kadlecay <matus.kadlecay@gmail.com>
2026-03-04 09:55:31 +0100

    refactor hide with stream logging

--- include/hide.h
+++ include/hide.h
@@ -10,6 +10,7 @@
  * @param to_hide data part to be hidden
  * @param id id of hidding
  * @param seq data part index, used to later extract data in order
+ * @param err output stream for error logging
  * 
  * @return `true` on success, `false` on failure
  */
@@ -17,7 +18,8 @@ bool hide_data(
     bmp::image &im,
     std::span<uint8_t> to_hide,
     uint8_t id,
-    uint8_t seq
+    uint8_t seq,
+    std::ostream &err
 );
 
 /**
@@ -25,10 +27,17 @@ bool hide_data(
  * 
  * @param images vector of images, where text will be hidden
  * @param data input stream of the message file
+ * @param out output stream for info logging
+ * @param err output stream for error logging
  * 
  * @return 0 on success, 1 if the full message could not be hidden
  * due to small images sizes, 2 in case of stream errors
  */
-int hide(std::vector<bmp::image> &images, std::istream &data);
+int hide(
+    std::vector<bmp::image> &images,
+    std::istream &data,
+    std::ostream &out = std::cout,
+    std::ostream &err = std::cerr
+);
 
 #endif  // HIDE_H
--- src/hide.cpp
+++ src/hide.cpp
@@ -9,17 +9,47 @@
 #include "../include/bitmap.h"
 #include "../include/hide.h"
 
+static void run_out_of_bytes_log(std::ostream &os, std::string_view filename) {
+    os << "image file " << filename << " is smaller than expected or there "
+          "is not enough space for altered image on disk\n";
+}
+
+static void data_size_error_log(
+    std::ostream &os,
+    std::size_t images_bytes_capacity,
+    std::size_t data_size
+) {
+    os << "only first " << images_bytes_capacity << " bytes were hidden, "
+          "please use more or larger images (" << data_size
+       << " byte capacity is needed)\n";
+}
+
+static void image_capacity_log(
+    std::ostream &os,
+    std::string_view filename,
+    std::size_t capacity
+) {
+    os << "image " << filename << " was opened with " << capacity
+       << " byte capacity\n";
+}
+
+static void image_not_necessary_log(
+    std::ostream &os,
+    std::string_view filename
+) {
+    os << "image " << filename << " was not necessary to hide data\n";
+}
+
 static bool hide_bytes(
     bmp::chunker &chnkr,
     bmp::image_buffer &buffer,
-    std::string_view image_filename
+    std::string_view image_filename,
+    std::ostream &err
 ) {
     uint8_t chunk;
     while (chnkr.get_chunk(chunk)) {
         if (!buffer.hide_chunk(chunk)) {
-            std::cerr << "image file " << image_filename << " is smaller "
-                      << "than expected or there is not enough space "
-                      << "for altered image on disk\n";
+            run_out_of_bytes_log(err, image_filename);
             return false;
         }
     }
@@ -30,7 +60,8 @@ bool hide_data(
     bmp::image &im,
     std::span<uint8_t> to_hide,
     uint8_t id,
-    uint8_t seq
+    uint8_t seq,
+    std::ostream &err
 ) {
     bmp::image_buffer buffer{im, MD_CHUNK_SIZE};
 
@@ -52,12 +83,12 @@ bool hide_data(
     bmp::chunker metadata_chnkr{
         std::span(metadata.data(), metadata.size()), MD_CHUNK_SIZE};
 
-    if (!hide_bytes(metadata_chnkr, buffer, im.filename))
+    if (!hide_bytes(metadata_chnkr, buffer, im.filename, err))
         return false;
 
     buffer.change_chunk_size(im.chunk_size);
     bmp::chunker data_chnkr{to_hide, im.chunk_size};
-    if (!hide_bytes(data_chnkr, buffer, im.filename))
+    if (!hide_bytes(data_chnkr, buffer, im.filename, err))
         return false;
 
     buffer.copy_rest();
@@ -71,8 +102,12 @@ static uint8_t generate_id() {
     return static_cast<uint8_t>(dist(e));
 }
 
-int hide(std::vector<bmp::image> &images, std::istream &data_in) {
-
+int hide(
+    std::vector<bmp::image> &images,
+    std::istream &data_in,
+    std::ostream &out,
+    std::ostream &err
+) {
     std::size_t data_size = data_in.seekg(0, std::ios::end).tellg();
     data_in.seekg(0, std::ios::beg);
 
@@ -87,25 +122,21 @@ int hide(std::vector<bmp::image> &images, std::istream &data_in) {
 
     for (; data_index < data_size && seq < images.size(); ++seq) {
         auto capacity = images[seq].byte_capacity();
-        std::cout << "image " << images[seq].filename
-                  << " was opened with "
-                  << capacity << " byte capacity\n";
+        image_capacity_log(out, images[seq].filename, capacity);
+
         auto sspan_size = std::min(capacity, data_size - data_index);
         std::span data_part = span.subspan(data_index, sspan_size);
 
-        if (!hide_data(images[seq], data_part, id, seq))
+        if (!hide_data(images[seq], data_part, id, seq, err))
             return 2;
         data_index += capacity;
     }
-    for (; seq < images.size(); ++seq) {
-        std::cout << "image " << images[seq].filename
-                  << " was not neccessary to hide data\n";
-    }
+
+    for (; seq < images.size(); ++seq)
+        image_not_necessary_log(out, images[seq].filename);
 
     if (data_index < data_size) {
-        std::cerr << "only first " << data_index << " bytes were hidden, "
-                  << "please use more or larger images ("
-                  << data_size << " capacity is needed)\n";
+        data_size_error_log(err, data_index, data_size);
         return 1;
     }
     return 0;
