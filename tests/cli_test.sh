25fad7b6cf9f6a24e4ce6d0d8c854448b8ed1252
Matúš Kadlecay <matus.kadlecay@gmail.com>
2026-03-05 20:29:05 +0100

    add cli test

--- tests/cli_test.sh
+++ tests/cli_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -f data/data_out
 ./build/sharky -c 4 bitmaps_in/image.bmp -c 8 bitmaps_in/image2.bmp -h -f data/data_in &&
 ./build/sharky bitmaps_out/image2.bmp bitmaps_out/image.bmp -e -f data/data_out
 cmp data/data_in data/data_out
