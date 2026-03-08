c64bac73ef18b991f0e8dec4bda18255a49f9815
Matúš Kadlecay <matus.kadlecay@gmail.com>
2026-03-08 20:14:49 +0100

    fix pipeline

--- tests/cli_test.sh
+++ tests/cli_test.sh
@@ -3,8 +3,18 @@ set -e
 
 rm -f data/data_out
 
-./build/sharky --hide --chunk_size 4 bitmaps_in/image.bmp --chunk_size 8 bitmaps_in/image2.bmp --file data/data_in
-./build/sharky --extract bitmaps_out/image2.bmp bitmaps_out/image.bmp --file data/data_out
+build/sharky \
+    --hide \
+    --chunk_size 4 \
+    bitmaps_in/image.bmp \
+    --chunk_size 8 \
+    bitmaps_in/image2.bmp \
+    --file data/data_in
+build/sharky \
+    --extract \
+    bitmaps_out/image2.bmp \
+    bitmaps_out/image.bmp \
+    --file data/data_out
 
 echo "Comparing input/output data..."
 cmp data/data_in data/data_out
